4e37d4428ffa80aae0e29f59061e6e318183e4d9
闫宇峥 <yanyuzheng@yanyuzhengdeMac-mini.local>
2023-04-26 17:42:15 +0800

    add database api

--- src/main/java/com/dsplab/bda/constants/SystemConstants.java
+++ src/main/java/com/dsplab/bda/constants/SystemConstants.java
@@ -16,4 +16,14 @@ public class SystemConstants {
 
     public static final String TOXICITY_PREDICTOR = "toxicityPredictor";
 
+    public static final String KEGG_LIST_URL = "https://rest.kegg.jp/list/reaction";
+
+    public static final String KEGG_INFO_URL = "https://rest.kegg.jp/get/";
+
+    public static final String BIGG_LIST_URL = "http://bigg.ucsd.edu/static/namespace/bigg_models_reactions.txt";
+
+    public static final String BIGG_INFO_URL = "http://bigg.ucsd.edu/api/v2/models/";
+
+    public static final String BIOCYC_INFO_URL = "https://websvc.biocyc.org/st-service-get";
+
 }
